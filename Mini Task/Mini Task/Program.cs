3d19ac23a73552d7621c065b2f8c9c239e99cdea
Vadim <lyamin.vadim.05@mail.ru>
2024-11-17 22:30:03 +0700

    Реализация функций

--- Mini Task/Mini Task/Program.cs
+++ Mini Task/Mini Task/Program.cs
@@ -1,24 +1,66 @@
 ﻿using System;
 using System.IO;
+using System.Net.NetworkInformation;
 namespace Mini_Task
 {
     public class MyTask
     {
         public static bool CheckLengthOfLineText(string txt, int numberOfSymbol)
         {
-            throw new Exception();
+            if (txt.Length > numberOfSymbol)
+                return false;
+            return true;
+        }
+        public static string[] ReadFile(string path)
+        {
+            return File.ReadAllText(path).Trim().Split('\n');
         }
 
         public static string RearrangingStringsInCenterOfSheet(string[] txt)
         {
-            throw new Exception();
+            int numberOfSymbol = parseInt(txt[0].Split(' ')[0]);
+            string result = ""; for (int i = 1; i < txt.Length; i++)
+            {
+                if (CheckLengthOfLineText(txt[i], numberOfSymbol))
+                {
+                    int lengthOfSpace = (numberOfSymbol - txt[i].Trim().Length) / 2;
+                    var SpacesForFirstPart = new string('+', lengthOfSpace);
+                    var SpacesForSecondPart = new string('+', (numberOfSymbol - txt[i].Trim().Length) - lengthOfSpace);
+                    result += SpacesForFirstPart + txt[i].Trim() + SpacesForSecondPart + '\n';
+                }
+                else
+                {
+                    result = "Impossible.";
+                    break;
+                }
+            }
+            return result;
+        }
+        public static int parseInt(string number)
+        {
+            if (!int.TryParse(number, out int nb))
+                throw new Exception("Число имеет неверный формат");
+            return Convert.ToInt16(number);
         }
 
+        public static void WritingToFile(string result, string path)
+        {
+            File.WriteAllText(path, result);
+        }
     }
     public class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string path1 = "input.txt"; string path2 = "output.txt";
+                MyTask.WritingToFile(MyTask.RearrangingStringsInCenterOfSheet(MyTask.ReadFile(path1)), path2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- Mini Task/TestsForMyMiniTask/UnitTest1.cs
+++ Mini Task/TestsForMyMiniTask/UnitTest1.cs
@@ -7,18 +7,34 @@ namespace TestsForMyMiniTask
         [Fact]
         public void Сhecking_whether_text_is_included_in_width_of_sheet()
         {
-            bool result = MyTask.CheckLengthOfLineText("Hello", 7);
-            Assert.True(result);
+            Assert.True(MyTask.CheckLengthOfLineText("Hello", 7));
+            Assert.False(MyTask.CheckLengthOfLineText("Hello", 4));
         }
 
 
         [Fact]
         public void Adding_spaces_at_beginning_and_at_end_of_string()
         {
-            string expected = "++++++Привет!+++++++\n++++Напиши+мне.+++++\n++++++Пока+=)+++++++";
+            string expected = "++++++Привет!+++++++\n++++Напиши мне.+++++\n++++++Пока =)+++++++\n";
             string[] parametr = { "20 3", "Привет!", "Напиши мне.", "Пока =)" };
             string result = MyTask.RearrangingStringsInCenterOfSheet(parametr);
             Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void checking_fot_the_correctness_of_int_parsing()
+        {
+            Assert.Equal(123, MyTask.parseInt("123"));
+            Assert.Equal(-123, MyTask.parseInt("-123"));
+            Assert.Equal(0, MyTask.parseInt("0"));
+        }
+
+        [Fact]
+        public void checking_fot_the_correctness_of_int_parsing_with_Exception()
+        {
+            Assert.Throws<Exception>(() => MyTask.parseInt("12,3"));
+            Assert.Throws<Exception>(() => MyTask.parseInt("sometext"));
+            Assert.Throws<Exception>(() => MyTask.parseInt(""));
         }
     }
 }
